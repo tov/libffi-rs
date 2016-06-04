276fbfda1f98eb4316f9213dcf29a55417145313
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-04 10:47:08 -0500

    Refactoring closure code into one macro instead of several.

--- src/high/mod.rs
+++ src/high/mod.rs
@@ -6,105 +6,52 @@ use middle;
 pub mod types;
 use self::types::*;
 
-macro_rules! declare_cif {
-    ( $typename:ident $( $param:ident )*) => {
+macro_rules! define_closure_types {
+    (
+        $cif:ident $callback:ident $callback_mut:ident
+                   $closure:ident  $closure_mut:ident ;
+                   $( $param:ident )*
+    )
+        =>
+    {
         /// Typed CIF (“Call InterFace”), which statically tracks
         /// argument and result types.
-        pub struct $typename<$( $param, )* R> {
+        pub struct $cif<$( $param, )* R> {
             untyped: middle::Cif,
             _marker: PhantomData<fn($( $param, )*) -> R>,
         }
 
-        impl<$( $param, )* R> $typename<$( $param, )* R> {
+        impl<$( $param, )* R> $cif<$( $param, )* R> {
             /// Creates a new statically-typed CIF from the given argument
             /// and result types.
             #[allow(non_snake_case)]
             pub fn new($( $param: Type<$param>, )* result: Type<R>) -> Self {
                 let cif = middle::Cif::new(vec![$( $param.into_untyped() ),*],
                                            result.into_untyped());
-                $typename { untyped: cif, _marker: PhantomData }
+                $cif { untyped: cif, _marker: PhantomData }
             }
         }
 
-        impl<$( $param: FfiType, )* R: FfiType> $typename<$( $param, )* R> {
+        impl<$( $param: FfiType, )* R: FfiType> $cif<$( $param, )* R> {
             /// Creates a new statically-typed CIF by reifying the
             /// argument types as `Type<T>`s.
             pub fn reify() -> Self {
                 Self::new($( $param::get_type(), )* R::get_type())
             }
         }
-    }
-}
 
-/// Typed Call InterFace for 0-argument functions.
-declare_cif!(Cif0);
-/// Typed Call InterFace for 1-argument functions.
-declare_cif!(Cif1 A);
-/// Typed Call InterFace for 2-argument functions.
-declare_cif!(Cif2 A B);
-/// Typed Call InterFace for 3-argument functions.
-declare_cif!(Cif3 A B C);
-/// Typed Call InterFace for 4-argument functions.
-declare_cif!(Cif4 A B C D);
-/// Typed Call InterFace for 5-argument functions.
-declare_cif!(Cif5 A B C D E);
-/// Typed Call InterFace for 6-argument functions.
-declare_cif!(Cif6 A B C D E F);
-/// Typed Call InterFace for 7-argument functions.
-declare_cif!(Cif7 A B C D E F G);
-/// Typed Call InterFace for 8-argument functions.
-declare_cif!(Cif8 A B C D E F G H);
-/// Typed Call InterFace for 9-argument functions.
-declare_cif!(Cif9 A B C D E F G H I);
-/// Typed Call InterFace for 10-argument functions.
-declare_cif!(Cif10 A B C D E F G H I J);
-
-macro_rules! declare_callback {
-    ( $typename:ident $( $param:ident )* ) => {
-        pub type $typename<U, $( $param, )* R>
+        pub type $callback<U, $( $param, )* R>
             = extern "C" fn(cif:      &::low::ffi_cif,
                             result:   &mut R,
                             args:     &($( &$param, )*),
                             userdata: &U);
-    }
-}
 
-declare_callback!( Callback0 );
-declare_callback!( Callback1 A );
-declare_callback!( Callback2 A B );
-declare_callback!( Callback3 A B C );
-declare_callback!( Callback4 A B C D );
-declare_callback!( Callback5 A B C D E );
-declare_callback!( Callback6 A B C D E F );
-declare_callback!( Callback7 A B C D E F G );
-declare_callback!( Callback8 A B C D E F G H );
-declare_callback!( Callback9 A B C D E F G H I );
-declare_callback!( Callback10 A B C D E F G H I J );
-
-macro_rules! declare_callback_mut {
-    ( $typename:ident $( $param:ident )* ) => {
-        pub type $typename<U, $( $param, )* R>
+        pub type $callback_mut<U, $( $param, )* R>
             = extern "C" fn(cif:      &::low::ffi_cif,
                             result:   &mut R,
                             args:     &($( &$param, )*),
                             userdata: &mut U);
-    }
-}
 
-declare_callback_mut!( CallbackMut0 );
-declare_callback_mut!( CallbackMut1 A );
-declare_callback_mut!( CallbackMut2 A B );
-declare_callback_mut!( CallbackMut3 A B C );
-declare_callback_mut!( CallbackMut4 A B C D );
-declare_callback_mut!( CallbackMut5 A B C D E );
-declare_callback_mut!( CallbackMut6 A B C D E F );
-declare_callback_mut!( CallbackMut7 A B C D E F G );
-declare_callback_mut!( CallbackMut8 A B C D E F G H );
-declare_callback_mut!( CallbackMut9 A B C D E F G H I );
-declare_callback_mut!( CallbackMut10 A B C D E F G H I J );
-
-macro_rules! declare_closure {
-    ( $closure:ident $cif:ident $callback:ident $( $param:ident )*) => {
         pub struct $closure<'a, $( $param, )* R> {
             untyped: middle::Closure<'a>,
             _marker: PhantomData<fn($( $param, )*) -> R>,
@@ -166,31 +113,15 @@ macro_rules! declare_closure {
                 Self::new_with_cif($cif::reify(), callback)
             }
         }
-    }
-}
 
-declare_closure!(Closure0 Cif0 Callback0);
-declare_closure!(Closure1 Cif1 Callback1 A);
-declare_closure!(Closure2 Cif2 Callback2 A B);
-declare_closure!(Closure3 Cif3 Callback3 A B C);
-declare_closure!(Closure4 Cif4 Callback4 A B C D);
-declare_closure!(Closure5 Cif5 Callback5 A B C D E);
-declare_closure!(Closure6 Cif6 Callback6 A B C D E F);
-declare_closure!(Closure7 Cif7 Callback7 A B C D E F G);
-declare_closure!(Closure8 Cif8 Callback8 A B C D E F G H);
-declare_closure!(Closure9 Cif9 Callback9 A B C D E F G H I);
-declare_closure!(Closure10 Cif10 Callback10 A B C D E F G H I J);
-
-macro_rules! declare_closure_mut {
-    ( $closure:ident $cif:ident $callback:ident $( $param:ident )*) => {
-        pub struct $closure<'a, $( $param, )* R> {
+        pub struct $closure_mut<'a, $( $param, )* R> {
             untyped: middle::Closure<'a>,
             _marker: PhantomData<fn($( $param, )*) -> R>,
         }
 
-        impl<'a, $( $param, )* R> $closure<'a, $( $param, )* R> {
-            pub fn from_parts<U>(cif: $cif<$( $param, )* R>,
-                                 callback: $callback<U, $( $param, )* R>,
+        impl<'a, $( $param, )* R> $closure_mut<'a, $( $param, )* R> {
+            pub fn from_parts<U>(cif:      $cif<$( $param, )* R>,
+                                 callback: $callback_mut<U, $( $param, )* R>,
                                  userdata: &'a mut U) -> Self
             {
                 let callback: middle::Callback<U, R>
@@ -199,7 +130,7 @@ macro_rules! declare_closure_mut {
                     = middle::Closure::new(cif.untyped,
                                            callback,
                                            userdata);
-                $closure {
+                $closure_mut {
                     untyped: closure,
                     _marker: PhantomData,
                 }
@@ -212,7 +143,7 @@ macro_rules! declare_closure_mut {
             }
         }
 
-        impl<'a, $( $param: Copy, )* R> $closure<'a, $( $param, )* R> {
+        impl<'a, $( $param: Copy, )* R> $closure_mut<'a, $( $param, )* R> {
             pub fn new_with_cif<Callback>(cif: $cif<$( $param, )* R>,
                                           callback: &'a mut Callback) -> Self
                 where Callback: FnMut($( $param, )*) -> R + 'a
@@ -236,7 +167,7 @@ macro_rules! declare_closure_mut {
         }
 
         impl<'a, $($param: Copy + FfiType,)* R: FfiType>
-            $closure<'a, $($param,)* R>
+            $closure_mut<'a, $($param,)* R>
         {
             pub fn new<Callback>(callback: &'a mut Callback) -> Self
                 where Callback: FnMut($( $param, )*) -> R + 'a
@@ -247,17 +178,32 @@ macro_rules! declare_closure_mut {
     }
 }
 
-declare_closure_mut!(ClosureMut0 Cif0 CallbackMut0);
-declare_closure_mut!(ClosureMut1 Cif1 CallbackMut1 A);
-declare_closure_mut!(ClosureMut2 Cif2 CallbackMut2 A B);
-declare_closure_mut!(ClosureMut3 Cif3 CallbackMut3 A B C);
-declare_closure_mut!(ClosureMut4 Cif4 CallbackMut4 A B C D);
-declare_closure_mut!(ClosureMut5 Cif5 CallbackMut5 A B C D E);
-declare_closure_mut!(ClosureMut6 Cif6 CallbackMut6 A B C D E F);
-declare_closure_mut!(ClosureMut7 Cif7 CallbackMut7 A B C D E F G);
-declare_closure_mut!(ClosureMut8 Cif8 CallbackMut8 A B C D E F G H);
-declare_closure_mut!(ClosureMut9 Cif9 CallbackMut9 A B C D E F G H I);
-declare_closure_mut!(ClosureMut10 Cif10 CallbackMut10 A B C D E F G H I J);
+define_closure_types!(Cif0 Callback0 CallbackMut0 Closure0 ClosureMut0;
+                      );
+define_closure_types!(Cif1 Callback1 CallbackMut1 Closure1 ClosureMut1;
+                      A);
+define_closure_types!(Cif2 Callback2 CallbackMut2 Closure2 ClosureMut2;
+                      A B);
+define_closure_types!(Cif3 Callback3 CallbackMut3 Closure3 ClosureMut3;
+                      A B C);
+define_closure_types!(Cif4 Callback4 CallbackMut4 Closure4 ClosureMut4;
+                      A B C D);
+define_closure_types!(Cif5 Callback5 CallbackMut5 Closure5 ClosureMut5;
+                      A B C D E);
+define_closure_types!(Cif6 Callback6 CallbackMut6 Closure6 ClosureMut6;
+                      A B C D E F);
+define_closure_types!(Cif7 Callback7 CallbackMut7 Closure7 ClosureMut7;
+                      A B C D E F G);
+define_closure_types!(Cif8 Callback8 CallbackMut8 Closure8 ClosureMut8;
+                      A B C D E F G H);
+define_closure_types!(Cif9 Callback9 CallbackMut9 Closure9 ClosureMut9;
+                      A B C D E F G H I);
+define_closure_types!(Cif10 Callback10 CallbackMut10 Closure10 ClosureMut10;
+                      A B C D E F G H I J);
+define_closure_types!(Cif11 Callback11 CallbackMut11 Closure11 ClosureMut11;
+                      A B C D E F G H I J K);
+define_closure_types!(Cif12 Callback12 CallbackMut12 Closure12 ClosureMut12;
+                      A B C D E F G H I J K L);
 
 #[cfg(test)]
 mod test {
