8df7493bddc9375da02327a039f6c38e8aca1daa
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-07 11:19:30 -0500

    More Builder docs

--- src/middle/builder.rs
+++ src/middle/builder.rs
@@ -3,7 +3,18 @@ use super::types::*;
 
 /// Provides a builder-style API for constructing CIFs and closures.
 ///
-/// Construct a builder using [`Builder::new`](#method.new).
+/// To use a builder, first construct it using [`Builder::new`](#method.new).
+/// The default calling convention is `FFI_DEFAULT_ABI`, and the default
+/// function type is `extern "C" fn()` (or in C, `void(*)()`). Add
+/// argument types to the function type with the [`arg`](#method.arg)
+/// and [`args`](#method.args) methods. Set the result type with
+/// [`res`](#method.res). Change the calling convention, if necessary,
+/// with [`abi`](#method.abi).
+///
+/// Once the builder is configured, construct a `Cif` with
+/// [`into_cif`](#method.into_cif) or a closure with
+/// [`into_closure`](#method.into_closure) or
+/// [`into_closure_mut`](#method.into_closure_mut).
 #[derive(Clone, Debug)]
 pub struct Builder {
     args: Vec<Type>,
@@ -45,12 +56,24 @@ impl Builder {
         self
     }
 
+    /// Builds a CIF.
     pub fn into_cif(self) -> super::Cif {
         let mut result = super::Cif::new(self.args.into_iter(), self.res);
         result.set_abi(self.abi);
         result
     }
 
+    /// Builds an immutable closure.
+    ///
+    /// # Arguments
+    ///
+    /// - `callback` — the function to call when the closure is invoked
+    /// - `userdata` — the pointer to pass to `callback` along with the
+    ///   arguments when the closure is called
+    ///
+    /// # Result
+    ///
+    /// The new closure.
     pub fn into_closure<'a, U, R>(
         self,
         callback: super::Callback<U, R>,
@@ -60,6 +83,17 @@ impl Builder {
         super::Closure::new(self.into_cif(), callback, userdata)
     }
 
+    /// Builds a mutable closure.
+    ///
+    /// # Arguments
+    ///
+    /// - `callback` — the function to call when the closure is invoked
+    /// - `userdata` — the pointer to pass to `callback` along with the
+    ///   arguments when the closure is called
+    ///
+    /// # Result
+    ///
+    /// The new closure.
     pub fn into_closure_mut<'a, U, R>(
         self,
         callback: super::CallbackMut<U, R>,
