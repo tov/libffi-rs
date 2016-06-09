eb141df195e9d61a24a2c1353ae2069180dcd7f7
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-09 00:27:54 -0500

    No longer installing libffi in /usr

--- install_libffi.sh
+++ install_libffi.sh
@@ -2,4 +2,4 @@
 set -ex
 wget ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz
 tar -zxf libffi-3.2.1.tar.gz
-cd libffi-3.2.1 && ./configure --prefix=/usr && make && sudo make install
+cd libffi-3.2.1 && ./configure && make && sudo make install
