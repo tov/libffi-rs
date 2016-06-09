5447d88fffae7fc749017efeafbb3829556522c0
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-09 11:22:28 -0500

    Cleaned up C libffi install script.

--- install_libffi.sh
+++ install_libffi.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+
+# For installing libffi on Travis.
+
 set -ex
+
 wget ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz
 tar -zxf libffi-3.2.1.tar.gz
-cd libffi-3.2.1 && ./configure && make && sudo make install
+cd libffi-3.2.1
+
+./configure
+make
+sudo make install
