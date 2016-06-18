5f897218a6e72014c4962faaec4633fec45cd193
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-17 19:07:43 -0500

    Updated build scripts.

--- bin/prepare_release.sh
+++ bin/prepare_release.sh
@@ -6,15 +6,26 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
-if git status --porcelain | grep .; then
-    echo >&2 Git status not clean.
+PROJ_ROOT="$(dirname "$0")/.."
+VERSION_FILE="$PROJ_ROOT"/.VERSION
+OLD_VERSION=$(cat "$VERSION_FILE")
+
+if [ "$VERSION" = "$OLD_VERSION" ]; then
+    echo >&2 "New version same as old: $VERSION"
     exit 2
 fi
 
-find process -type f | sed 's@process/@@' | while read file; do
-    sed "s/@VERSION@/$VERSION/" "process/$file" > "$file"
-    git add "$file"
-done
+if git status --porcelain | grep .; then
+    echo >&2 Git status not clean.
+    exit 3
+fi
+
+rm -f "$VERSION_FILE"
+echo "$VERSION" > "$VERSION_FILE"
+chmod a-w "$VERSION_FILE"
+git add "$VERSION_FILE"
+
+"$PROJ_ROOT"/bin/preprocess.sh
 
 git ci -m "Version: $VERSION"
 git tag v$VERSION
