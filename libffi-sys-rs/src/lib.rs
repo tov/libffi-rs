bad93043a347bdb0eefd0823bb386cc35108b993
Yorick Peterse <yorick@yorickpeterse.com>
2021-05-06 21:49:44 +0200

    (cargo-release) version 1.1.1

--- libffi-sys-rs/src/lib.rs
+++ libffi-sys-rs/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/libffi-sys/1.1.0")]
+#![doc(html_root_url = "https://docs.rs/libffi-sys/1.1.1")]
 //! Low-level Rust bindings for [libffi](https://sourceware.org/libffi/)
 //!
 //! The C libffi library provides two main facilities: assembling calls
@@ -26,14 +26,14 @@
 //!
 //! ```toml
 //! [dependencies]
-//! libffi-sys = "1.1.0"
+//! libffi-sys = "1.1.1"
 //! ```
 //!
 //! to your `Cargo.toml`. If you want to use your system C libffi, then
 //!
 //! ```toml
 //! [dependencies.libffi-sys]
-//! version = "1.1.0"
+//! version = "1.1.1"
 //! features = ["system"]
 //! ```
 //!
