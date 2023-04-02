77773032d950517621cd730c4e7cf4e6ed2703c2
Yorick Peterse <git@yorickpeterse.com>
2023-04-02 23:28:50 +0200

    chore: Release

--- libffi-sys-rs/README.md
+++ libffi-sys-rs/README.md
@@ -31,14 +31,14 @@ a C libffi for you, add
 
 ```toml
 [dependencies]
-libffi-sys = "2.2.0"
+libffi-sys = "2.2.1"
 ```
 
 to your `Cargo.toml`. If you want to use your system C libffi, then
 
 ```toml
 [dependencies.libffi-sys]
-version = "2.2.0"
+version = "2.2.1"
 features = ["system"]
 ```
 
--- libffi-sys-rs/src/lib.rs
+++ libffi-sys-rs/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/libffi-sys/2.2.0")]
+#![doc(html_root_url = "https://docs.rs/libffi-sys/2.2.1")]
 //! Low-level Rust bindings for [libffi](https://sourceware.org/libffi/)
 //!
 //! The C libffi library provides two main facilities: assembling calls
@@ -24,14 +24,14 @@
 //!
 //! ```toml
 //! [dependencies]
-//! libffi-sys = "2.2.0"
+//! libffi-sys = "2.2.1"
 //! ```
 //!
 //! to your `Cargo.toml`. If you want to use your system C libffi, then
 //!
 //! ```toml
 //! [dependencies.libffi-sys]
-//! version = "2.2.0"
+//! version = "2.2.1"
 //! features = ["system"]
 //! ```
 //!
